193c610e875596930241f1e799041d7bcf957cf4
Ben Hall <benji2357@gmail.com>
2017-07-16 09:31:06 +0100

    chore(webpack): Use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+  },
   entry: './src/index.js',
   module: {
     rules: [
@@ -22,6 +26,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new CopyWebpackPlugin([{from: 'src/index.html'}]),
     new webpack.optimize.ModuleConcatenationPlugin(),
   ],
 }
