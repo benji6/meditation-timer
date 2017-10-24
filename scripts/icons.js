3d57d8de67d0ec70b32df2986cc5fbf9bf02efe3
Ben Hall <benji2357@gmail.com>
2017-10-24 22:22:44 +0100

    feat(icons): New app icon

---
+++ scripts/icons.js
@@ -0,0 +1,46 @@
+const favicons = require('favicons')
+const fs = require('fs')
+const path = require('path')
+
+const sizes = [
+  '144x144',
+  '192x192',
+  '512x512',
+]
+
+const source = 'src/assets/icons/icon.svg'
+
+const configuration = {
+  online: false,
+  preferOnline: false,
+  icons: {
+    android: true,
+    appleIcon: false,
+    appleStartup: false,
+    coast: false,
+    favicons: true,
+    firefox: false,
+    windows: false,
+    yandex: false
+  }
+}
+
+favicons(source, configuration, (err, response) => {
+  if (err) throw err
+
+  response.images
+    .filter(({name}) => {
+      if (name.includes('favicon.ico')) return true
+      for (const size of sizes) if (name.includes(size)) return true
+      return false
+    })
+    .map(({contents, name}) => ({
+      contents,
+      name: name.replace('android-chrome', 'icon')
+    }))
+    .forEach(({contents, name}) => fs.writeFile(
+      path.join(__dirname, '..', 'src', 'assets', 'icons', name),
+      contents,
+      err => { if (err) throw err }
+    ))
+})
