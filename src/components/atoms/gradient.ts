7ef49c0472740869191ef132e18abb703c17eee3
Ben Hall <benji2357@gmail.com>
2017-11-16 20:54:08 +0000

    fix(gradient): No more gradient weirdness when moving between about and timer

--- src/components/atoms/gradient.ts
+++ src/components/atoms/gradient.ts
@@ -1,23 +1,15 @@
 import './gradient.css'
 
-const gradient0El = document.querySelector('.gradient__0') as HTMLDivElement
-const gradient1El = document.querySelector('.gradient__1') as HTMLDivElement
-const gradient2El = document.querySelector('.gradient__2') as HTMLDivElement
+const gradientEl = document.querySelector('.gradient') as HTMLDivElement
 
 class Gradient {
   setGradient (n: number) {
-    if (n === 0) {
-      gradient0El.classList.remove('gradient__0--hidden')
-      gradient2El.classList.add('gradient__2--hidden')
-    } else if (n === 1) {
-      gradient0El.classList.add('gradient__0--hidden')
-      gradient1El.classList.remove('gradient__1--hidden')
-      gradient2El.classList.add('gradient__2--hidden')
-    } else {
-      gradient0El.classList.add('gradient__0--hidden')
-      gradient1El.classList.add('gradient__1--hidden')
-      gradient2El.classList.remove('gradient__2--hidden')
-    }
+    const newLayerEl = document.createElement('div')
+    newLayerEl.classList.add('gradient__layer', `gradient__layer--${n}`)
+    newLayerEl.addEventListener('animationend', () => {
+      (document.querySelector('.gradient__layer') as HTMLDivElement).remove()
+    })
+    gradientEl.appendChild(newLayerEl)
   }
 }
 
