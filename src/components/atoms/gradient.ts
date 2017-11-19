3487f9f93c0ea62c3c1244a715a56cc6756d6065
Ben Hall <benji2357@gmail.com>
2017-11-19 21:49:53 +0000

    feat(gradient): Gradient now rotates slowly

--- src/components/atoms/gradient.ts
+++ src/components/atoms/gradient.ts
@@ -1,6 +1,26 @@
 import './gradient.css'
+import {getCssVar, setCssVar} from '../../utils'
+
+const rotationInterval = 1000 * 60 * 5
 
 const gradientEl = document.querySelector('.gradient') as HTMLDivElement
+
+let gradientAngle = getCssVar('--gradient-angle')
+
+const render = () => {
+  requestAnimationFrame(render)
+
+  const ratioOfDayCompleted = Date.now() % rotationInterval / rotationInterval
+  const newGradientAngle = (ratioOfDayCompleted * 360).toFixed(0) + 'deg'
+
+  if (newGradientAngle === gradientAngle) return
+
+  gradientAngle = newGradientAngle
+
+  setCssVar('--gradient-angle', gradientAngle)
+}
+
+render()
 
 class Gradient {
   setGradient (n: number) {
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,4 @@ const {documentElement} = document
 const rootStyles = getComputedStyle(documentElement)
 
 export const getCssVar = (key: string) => rootStyles.getPropertyValue(key).trim()
+export const setCssVar = (key: string, val: string) => documentElement.style.setProperty(key, val)
