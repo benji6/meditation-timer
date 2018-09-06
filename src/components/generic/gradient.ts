d4fccefa2594f55a28653ce4ec397213aebdb789
Ben Hall <benji2357@gmail.com>
2018-09-06 20:02:58 +0100

    feat(gradient): gradient no longer rotates - kinder on batteries :)

--- src/components/generic/gradient.ts
+++ src/components/generic/gradient.ts
@@ -1,26 +1,6 @@
-import { getCssVar, setCssVar } from '../../utils'
 import './gradient.css'
 
-const rotationInterval = 1000 * 60 * 30
-
 const gradientEl = document.querySelector('.gradient') as HTMLDivElement
-
-let gradientAngle = getCssVar('--gradient-angle')
-
-const render = () => {
-  requestAnimationFrame(render)
-
-  const ratioOfDayCompleted = (Date.now() % rotationInterval) / rotationInterval
-  const newGradientAngle = (ratioOfDayCompleted * 360).toFixed(0) + 'deg'
-
-  if (newGradientAngle === gradientAngle) return
-
-  gradientAngle = newGradientAngle
-
-  setCssVar('--gradient-angle', gradientAngle)
-}
-
-render()
 
 type GradientRange = 0 | 1 | 2 | 3 | 4
 
--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,3 @@ const rootStyles = getComputedStyle(documentElement)
 
 export const getCssVar = (key: string) =>
   rootStyles.getPropertyValue(key).trim()
-export const setCssVar = (key: string, val: string) =>
-  documentElement.style.setProperty(key, val)
